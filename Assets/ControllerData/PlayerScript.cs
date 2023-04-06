ff2fe08659f90718ecda8096db95266b6a41badf
BlazingBlitzi <114948273+BlazingBlitzi@users.noreply.github.com>
2023-04-06 10:50:01 -0500

    hendricksonTristinCommit11

--- Assets/ControllerData/PlayerScript.cs
+++ Assets/ControllerData/PlayerScript.cs
@@ -8,24 +8,23 @@ public class PlayerScript : MonoBehaviour
     InputActionAsset inputAsset;
     InputActionMap inputMap;
     InputAction move;
-    InputAction look;
+    InputAction rotate;
 
     Vector2 movement;
-    Vector3 lookAround;
+    Vector2 rotation;
 
-    // Start is called before the first frame update
     private void Awake()
     {
         inputAsset = this.GetComponent<PlayerInput>().actions;
         inputMap = inputAsset.FindActionMap("PlayerActionMap");
         move = inputMap.FindAction("Move");
-        look = inputMap.FindAction("Look");
+        rotate = inputMap.FindAction("Rotate");
 
-        move.performed += contx => movement = contx.ReadValue<Vector2>();
-        move.canceled += contx => movement = Vector2.zero;
+        move.performed += ctx => movement = ctx.ReadValue<Vector2>();
+        move.canceled += ctx => movement = Vector2.zero;
 
-        look.performed += contx => lookAround = contx.ReadValue<Vector2>();
-        look.canceled += contx => lookAround = Vector2.zero;
+        rotate.performed += ctx => rotation = ctx.ReadValue<Vector2>();
+        rotate.canceled += ctx => rotation = Vector2.zero;
     }
 
     private void FixedUpdate()
@@ -33,11 +32,12 @@ public class PlayerScript : MonoBehaviour
         Vector2 movementVelocity = new Vector2(movement.x, movement.y) * 5f * Time.deltaTime;
         transform.Translate(movementVelocity, Space.Self);
 
-        Vector2 lookAroundVelocity = new Vector2(-lookAround.y, lookAround.x) * 5f * Time.deltaTime;
-        transform.Rotate(lookAroundVelocity, Space.Self);
+        Vector2 rotationVelocity = new Vector2(rotation.x, rotation.y);
+        Quaternion targetRotation = Quaternion.LookRotation(transform.forward, rotationVelocity);
+        Quaternion rotateTowards = Quaternion.RotateTowards(transform.rotation, targetRotation, 1f * Time.deltaTime);
+        transform.Rotate(rotateTowards.eulerAngles);
     }
 
-    // Update is called once per frame
     private void OnEnable()
     {
         inputMap.Enable();
